78ec7ac1a6f304c905a8e4fa6fb927d2403b6ef6
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-03-09 00:14:50 +0900

    activation sigmoid, relu

--- ch3. Neural Network/activation_functions.py
+++ ch3. Neural Network/activation_functions.py
@@ -3,6 +3,12 @@ import numpy as np
 def step_function(x):
     return np.array(x > 0, dtype = np.int32)
 
+def sigmoid(x):
+    return 1 / (np.exp(-x)+1)
+
+def relu(x):
+    return np.maximum(0, x)
+
 # step test
 a = np.array([1, -1])
 print(a)
@@ -14,3 +20,11 @@ print(step_function(1))
 print(step_function(2))
 print(step_function(0))
 print(step_function(-1))
+
+# sigmoid test
+print(sigmoid(1))
+print(sigmoid(np.array([0,2, -2])))
+
+# relu test
+print(relu(0))
+print(relu(np.array([-3,-1,1,4])))
