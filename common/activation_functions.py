e088f48145cd24fd8e01fd14b0524418d800cc9f
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-03-23 22:08:12 +0900

    common activation_functions

---
+++ common/activation_functions.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def step_function(x):
+    return np.array(x > 0, dtype = np.int32)
+
+def sigmoid(x):
+    return 1 / (np.exp(-x)+1)
+
+def relu(x):
+    return np.maximum(0, x)
+
+def identity(x):
+    return x
+
+def softmax(x):
+    c = np.max(x)
+    exp_x = np.exp(x-c)
+    sum_exp_x = np.sum(exp_x)
+    return exp_x / sum_exp_x
