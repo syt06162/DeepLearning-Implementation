c9cbbfbbb48fb67810f8ad69d7862a5827b8a19f
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-04-04 00:35:11 +0900

    loss_functions : CEE batch

--- ch4. Neural Network Learning/loss_functions.py
+++ ch4. Neural Network Learning/loss_functions.py
@@ -6,6 +6,18 @@ def sum_of_squared_error(y, t):
 def mean_squared_error(y, t):
     return np.sum((y-t)**2) / len(y) 
 
+# # (no batch) cross_entropy_error
+# def cross_entropy_error(y, t):
+#     delta = 1e-7
+#     return -np.sum(t * np.log(y + delta))
+
+# (batch, normalized) cross_entropy_error 
 def cross_entropy_error(y, t):
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+    if y.ndim == 1:
+        y.reshape(1, y.shape[0])
+        t.reshape(1, t.shape[0])
+    
+    batch_size = y.shape[0]
+    delta = 1e-7  
+    return -np.sum(t * np.log(y + delta)) / batch_size
+
