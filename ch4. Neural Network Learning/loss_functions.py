6362fb3ddfb7ec014656035ec4a852bea0be48de
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-04-04 00:49:50 +0900

    loss_functions: test

--- ch4. Neural Network Learning/loss_functions.py
+++ ch4. Neural Network Learning/loss_functions.py
@@ -21,3 +21,18 @@ def cross_entropy_error(y, t):
     delta = 1e-7  
     return -np.sum(t * np.log(y + delta)) / batch_size
 
+
+# test
+if __name__ == "__main__":
+
+    y = [0.10, 0.05, 0.00, 0.60, 0.05, 0.10, 0.00, 0.00, 0.10, 0.0]
+    t = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+
+    # SSE
+    print("SSE:", sum_of_squared_error(np.array(y), np.array(t)))
+
+    # MSE
+    print("MSE:", mean_squared_error(np.array(y), np.array(t)))
+
+    # CEE
+    print("CEE:", cross_entropy_error(np.array(y), np.array(t)))
