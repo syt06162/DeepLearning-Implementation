435b426b4edf49c6042b3271dabf6a25e35c79f7
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-04-05 20:42:30 +0900

    numerical gradient descent

--- ch4. Neural Network Learning/numerical_gradient_descent.py
+++ ch4. Neural Network Learning/numerical_gradient_descent.py
@@ -20,13 +20,35 @@ def numerical_gradient(f, x):
         
     return gradient
 
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+    
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x = x - lr*grad
+    
+    return x
+
 
 # test
 if __name__ == "__main__":
 
+    # numerical gradient
     def fun_1(x):
         return x[0]**2 + x[1]**2
     
-    print(numerical_gradient(fun_1, np.array([3.0, 4.0])))  
-    print(numerical_gradient(fun_1, np.array([0.0, 2.0])))
-    
+    print("numerical_gradient: ")
+    print("f(x0, x1) = x0² + x1²")
+    print([3.0, 4.0], "->" , numerical_gradient(fun_1, np.array([3.0, 4.0])))  
+    print([0.0, 2.0], "->" , numerical_gradient(fun_1, np.array([0.0, 2.0])))
+    print()
+
+    # gradient descent
+    print("gradient_descent: ")
+    print("f(x0, x1) = x0² + x1²")
+    start_position = [-3.0, 4.0]
+    lr = 0.001
+    print(start_position, "lr=" + str(lr), "->" , gradient_descent(fun_1, np.array(start_position), lr=lr))  
+    lr = 1
+    print(start_position, "lr=" + str(lr), "->" , gradient_descent(fun_1, np.array(start_position), lr=lr))
+    print()
