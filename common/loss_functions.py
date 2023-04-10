5235ca9715350ffa8afb77c7d89471316027d841
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-04-10 11:03:37 +0900

    common : loss function

---
+++ common/loss_functions.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def sum_of_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+def mean_squared_error(y, t):
+    return np.sum((y-t)**2) / len(y) 
+
+# (batch, normalized) cross_entropy_error 
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        y.reshape(1, y.shape[0])
+        t.reshape(1, t.shape[0])
+    
+    batch_size = y.shape[0]
+    delta = 1e-7  
+    return -np.sum(t * np.log(y + delta)) / batch_size
