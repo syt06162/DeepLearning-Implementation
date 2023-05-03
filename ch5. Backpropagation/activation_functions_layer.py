6dd5cfbddca72c11328015226510e11440027a60
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-05-03 14:28:32 +0900

    ch5/ activation layer: softmax_with_loss, test

--- ch5. Backpropagation/activation_functions_layer.py
+++ ch5. Backpropagation/activation_functions_layer.py
@@ -1,4 +1,13 @@
 import numpy as np
+import os, sys
+
+### important! 중요!
+# If the current working directory is ch4, please work from the parent directory.
+parent_path = os.getcwd()
+sys.path.append(parent_path) 
+
+from common.activation_functions import softmax, sigmoid
+from common.loss_functions import cross_entropy_error
 
 class Relu:
     def __init__(self):
@@ -20,7 +29,7 @@ class Sigmoid:
         self.out = None
 
     def forward(self, x):
-        self.out = 1 / (np.exp(-x)+1)
+        self.out = sigmoid(x)
         return self.out
     
     def backward(self, dout):
@@ -44,7 +53,24 @@ class Affine:
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
         return dx
+    
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
 
+    def forward(self, x, t):
+        self.y = softmax(x)
+        self.t = t
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+    
+    def backward(self, dout=1): # don't use
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        
+        return dx
 
 
 if __name__ == "__main__":
@@ -52,28 +78,28 @@ if __name__ == "__main__":
     # relu test
     test = [-2, -1, 3, 10]
     x = np.array(test)
-    relu = Relu()
+    relu_test = Relu()
 
     print("# relu test")
     print(test)
     print("forward:")
-    print(relu.forward(x))
+    print(relu_test.forward(x))
     print("backward:")
-    print(relu.backward(x))
+    print(relu_test.backward(x))
     print()
 
 
     # sigmoid test
     test = [-2, -1, 3, 10]
     x = np.array(test)
-    sigmoid = Sigmoid()
+    sigmoid_test = Sigmoid()
 
     print("# sigmoid test")
     print(test)
     print("forward:")
-    print(sigmoid.forward(x))
+    print(sigmoid_test.forward(x))
     print("backward:")
-    print(sigmoid.backward(x))
+    print(sigmoid_test.backward(x))
     print()
 
 
@@ -81,15 +107,29 @@ if __name__ == "__main__":
     x = np.array( [[1,2], [10,20] ]) # batch
     W = np.array( [[3,4,5] , [6,7,8]] )
     b = np.array( [10,11,12] )
-    affine = Affine(W,b)
+    affine_test = Affine(W,b)
 
     print("# affine test")
     print(x)
     print("forward:")
-    y = affine.forward(x)
+    y = affine_test.forward(x)
     print(y)
     print("backward:")
-    print(affine.backward(y))
+    print(affine_test.backward(y))
     print()
 
-    
+
+    # softmax-with-loss test
+    test = [-2, -1, 3, 4]
+    true = [0, 0, 0, 1]
+    x = np.array(test)
+    t = np.array(true)
+    softmax_with_loss_test = SoftmaxWithLoss()
+
+    print("# softmax_with_loss test")
+    print(test)
+    print("forward:")
+    print(softmax_with_loss_test.forward(x, t))
+    print("backward:")
+    print(softmax_with_loss_test.backward(x))
+    print()
