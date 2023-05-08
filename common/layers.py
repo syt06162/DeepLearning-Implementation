22c91e7d6297760dcb6598586214933df704e0c4
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-05-08 13:14:21 +0900

    common layers : relu, sigmoid, affine, softmaxwithloss (same as ch5)

---
+++ common/layers.py
@@ -0,0 +1,74 @@
+import numpy as np
+import os, sys
+
+### important! 중요!
+# If the current working directory is ch4, please work from the parent directory.
+parent_path = os.getcwd()
+sys.path.append(parent_path) 
+
+from common.activation_functions import softmax, sigmoid
+from common.loss_functions import cross_entropy_error
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+    
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+    
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        return dx
+    
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        self.out = sigmoid(x)
+        return self.out
+    
+    def backward(self, dout):
+        dx = dout * (self.out) * (1 - self.out)
+        return dx
+    
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+    
+    def forward(self, x):
+        self.x = x
+        return np.dot(x, self.W) + self.b
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        return dx
+    
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.y = softmax(x)
+        self.t = t
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+    
+    def backward(self, dout=1): # don't use
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        
+        return dx
+
