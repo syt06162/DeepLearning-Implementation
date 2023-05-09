4b1c995aa57040dd956555848cb2e9242fde4e6f
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-05-10 00:03:10 +0900

    ch6/ optimizers: Adagrad

--- ch6. Learning Strategy/optimizers.py
+++ ch6. Learning Strategy/optimizers.py
@@ -24,3 +24,20 @@ class Momentum:
         for key in params.keys():
             self.v[key] = self.v[key] * self.momentum - self.lr * grads[key]
             params[key] += self.v[key]
+
+class Adagrad:
+    def __init__(self, lr = 0.01):
+        self.lr = lr
+        self.h = None
+    
+    def update(self, params, grads):
+        # h initialize
+        if self.h == None:
+            self.h = {}
+            for key, value in params.items():
+                self.h[key] = np.zeros_like(value)
+
+        for key in params.keys():
+            self.h[key] +=grads[key] * grads[key]
+            params[key] -= self.lr*grads[key] / (np.sqrt(self.h[key]) + 1e-7) # avoid zero division
+
