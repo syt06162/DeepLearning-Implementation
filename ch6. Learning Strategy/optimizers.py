e5ecf7734412a1b4a7c431897376b99ae8a29680
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-05-16 01:19:00 +0900

    ch6/ optimizers: adam

--- ch6. Learning Strategy/optimizers.py
+++ ch6. Learning Strategy/optimizers.py
@@ -55,6 +55,38 @@ class RMSprop:
                 self.h[key] = np.zeros_like(value)
 
         for key in params.keys():
-            # h(t) <- (D) * h(t-1) + (1-D)(dL*dL)
+            # h(t) <- (D) * h(t-1) + (1-D) * (dL*dL)
             self.h[key] = self.decay_rate * self.h[key] + (1 - self.decay_rate) * (grads[key] * grads[key])
             params[key] -= self.lr*grads[key] / (np.sqrt(self.h[key]) + 1e-7) # avoid zero division
+
+class Adam:
+    """ beta1 for momentum, beta2 for adagrad """
+    def __init__(self, lr = 0.001, beta1=0.9, beta2=0.999):
+        self.lr = lr
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.m = None
+        self.v = None
+
+        # iter?
+        self.iter = 0
+    
+    def update(self, params, grads):
+        # m, v initialize
+        if self.m == None:
+            self.m , self.v = {}, {}
+            for key, value in params.items():
+                self.m[key] = np.zeros_like(value)
+                self.v[key] = np.zeros_like(value)
+
+        # iter
+        self.iter += 1
+        lr_t = self.lr * np.sqrt(1.0 - self.beta2**self.iter) / (1.0 - self.beta1**self.iter)
+
+        for key in params.keys():
+            # m(t) <- (b1) * m(t-1) + (1-b1) * (dL)
+            # v(t) <- (b2) * v(t-1) + (1-b2) * (dL*dL)
+            self.m[key] = self.beta1 * (self.m[key]) + (1 - self.beta1) * grads[key]
+            self.v[key] = self.beta2 * (self.v[key]) + (1 - self.beta2) * grads[key] * grads[key]
+            # params
+            params[key] -= lr_t * self.m[key] / (np.sqrt(self.v[key]) + 1e-7) # avoid zero division
