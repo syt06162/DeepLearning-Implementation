f1bf2285d0f201842738684ca8c6ee4fdc070d37
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-05-15 21:55:49 +0900

    ch6/ optimizers: RMSprop

--- ch6. Learning Strategy/optimizers.py
+++ ch6. Learning Strategy/optimizers.py
@@ -38,6 +38,23 @@ class Adagrad:
                 self.h[key] = np.zeros_like(value)
 
         for key in params.keys():
-            self.h[key] +=grads[key] * grads[key]
+            self.h[key] += grads[key] * grads[key]
             params[key] -= self.lr*grads[key] / (np.sqrt(self.h[key]) + 1e-7) # avoid zero division
 
+class RMSprop:
+    def __init__(self, lr = 0.01, decay_rate=0.99):
+        self.lr = lr
+        self.h = None
+        self.decay_rate = decay_rate
+    
+    def update(self, params, grads):
+        # h initialize
+        if self.h == None:
+            self.h = {}
+            for key, value in params.items():
+                self.h[key] = np.zeros_like(value)
+
+        for key in params.keys():
+            # h(t) <- (D) * h(t-1) + (1-D)(dL*dL)
+            self.h[key] = self.decay_rate * self.h[key] + (1 - self.decay_rate) * (grads[key] * grads[key])
+            params[key] -= self.lr*grads[key] / (np.sqrt(self.h[key]) + 1e-7) # avoid zero division
