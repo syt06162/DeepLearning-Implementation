0dbeb1543a270ab6e2ae3eeff5bc734d36979f09
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-05-19 18:49:01 +0900

    common/ layers: dropout

--- common/layers.py
+++ common/layers.py
@@ -76,3 +76,21 @@ class SoftmaxWithLoss:
         
         return dx
 
+class Dropout:
+    """
+    reference:
+    http://arxiv.org/abs/1207.0580
+    """
+    def __init__(self, dropout_ratio=0.5):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+
+    def forward(self, x, train_flg=True):
+        if train_flg:
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            return x * self.mask # false -> dropout
+        else:
+            return x * (1.0-self.dropout_ratio)
+
+    def backward(self, dout):
+        return dout * self.mask 
